1da65ded4d5d445e9ef21ad3bcb50860c4d6beeb
softtiger <revokeyu@hotmail.com>
2022-07-12 21:44:54 +0800

    use logback

--- src/main/java/self/eshop/services/impl/ProductServices.java
+++ src/main/java/self/eshop/services/impl/ProductServices.java
@@ -2,6 +2,8 @@ package self.eshop.services.impl;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import self.eshop.dao.ProductCategoryMapper;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @Service
 public class ProductServices implements IProductServices {
+    static  final Logger logger = LoggerFactory.getLogger(ProductServices.class);
 
     @Autowired
     private ProductCategoryMapper productCategoryDao;
@@ -25,10 +28,11 @@ public class ProductServices implements IProductServices {
 
     @Override
     public List<ProductCategory> getAll(int pageNo, int pageSize, String categoryName) {
+        logger.info("request parameter---pageNo:{}, pageSize:{},categoryName:{}",pageNo,pageSize,categoryName);
         PageHelper.startPage(pageNo,pageSize);
         List<ProductCategory> productCategoryList = productCategoryDao.findAll(categoryName);
         PageInfo<ProductCategory> pageInfo = new PageInfo<>(productCategoryList);
-        System.out.printf("total page:%d,total number:%d", pageInfo.getPages(),pageInfo.getTotal());
+        logger.info("total page:{},total number:{}", pageInfo.getPages(),pageInfo.getTotal());
         return productCategoryList;
     }
 }
